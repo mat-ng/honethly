1f65f5e84589f6e05e69ea4dabada4c0fa57a829
Alvin787 <alvinhalo4@gmail.com>
2023-09-16 12:05:36 -0400

    added business cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,30 @@ import React from 'react';
 import Header from './../components/ui/header';
 import SearchBar from './../components/ui/search';
 import BusinessCard from './../components/ui/card';
+import { Button } from "@/components/ui/button";
+import Business1img from './../../img/preview.jpg';
 
 const Homepage: React.FC = () => {
   // Sample data (replace with your data fetching logic)
   const businesses = [
-    { name: 'Business 1', description: 'Description for Business 1' },
-    { name: 'Business 2', description: 'Description for Business 2' },
-    // Add more business data as needed
+    {
+      name: 'Business 1',
+      description: 'Description for Business 1',
+      rating: 4.5, // Sample rating value
+      imageUrl: Business1img.src // Sample image source URL
+    },
+    {
+      name: 'Business 2',
+      description: 'Description for Business 2',
+      rating: 3.7, // Sample rating value
+      imageUrl: Business1img.src // Sample image source URL
+    },
   ];
 
   return (
     <div className="min-h-screen flex flex-col justify-center bg-gray-100 dark:bg-gray-800">
       <Header />
+      
       <SearchBar />
       <div className="container mx-auto p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,14 +3,21 @@ import React from 'react';
 interface BusinessCardProps {
   name: string;
   description: string;
-  // Add more business-related fields as needed
+  rating: number;
+  imageUrl: string;
 }
 
-const BusinessCard: React.FC<BusinessCardProps> = ({ name, description }) => {
+const BusinessCard: React.FC<BusinessCardProps> = ({ name, description, rating, imageUrl }) => {
   return (
-    <div className="business-card">
-      <h2>{name}</h2>
+    <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-4">
+      <div className="mb-4">
+        <img src={imageUrl} alt={name} className="w-full h-auto rounded-lg" />
+      </div>
+      <h2 className="text-xl font-semibold mb-2">{name}</h2>
       <p>{description}</p>
+      <div className="mt-2 flex items-center">
+        <span className="text-yellow-500">{rating} stars</span>
+      </div>
     </div>
   );
 };
