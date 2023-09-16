7d5ce5a3d2cda120b2ad68afd3cb0c65a46ca8fd
Alvin Qian <alvinhalo4@gmail.com>
2023-09-16 16:01:34 -0400

    changed colour scheme and added some sample images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import SearchBar from './../components/ui/search';
 import BusinessCard from './../components/ui/card';
 import Business1img from './../../img/preview.jpg';
 import {BusinessModal} from "@/components/ui/business";
+import campuspizza from './../../img/campuspizza.jpg';
+import music from './../../img/music.jpg';
+import pharmasave from './../../img/pharmasave.jpg';
 
 export interface Business {
   name: string;
@@ -17,24 +20,24 @@ const Homepage: React.FC = () => {
   // Sample data (replace with your data fetching logic)
   const businesses: Business[] = [
     {
-      name: 'Business 1',
-      description: 'Description for Business 1',
+      name: 'Campus Pizza',
+      description: 'Pizza',
       rating: 4.5, // Sample rating value
-      imageUrl: Business1img.src, // Sample image source URL
+      imageUrl: campuspizza.src, // Sample image source URL
       ethAddress:'0x853A2208e924466ff7048E2878eaa596e9868a4C',
     } as Business,
     {
-      name: 'Business 2',
-      description: 'Description for Business 2',
+      name: 'Long & McQuade',
+      description: 'Musical Instruments',
       rating: 3.7, // Sample rating value
-      imageUrl: Business1img.src,// Sample image source URL,
+      imageUrl: music.src,// Sample image source URL,
       ethAddress:'0xE650ac792a9244dd9E97e37548a1123D9ba27003',
     } as Business,
     {
-      name: 'Business 2',
-      description: 'Description for Business 2',
+      name: 'Pharmasave Westmount Place ',
+      description: 'Pharmacy & Home Health Care',
       rating: 3.7, // Sample rating value
-      imageUrl: Business1img.src,// Sample image source URL,
+      imageUrl: pharmasave.src,// Sample image source URL,
       ethAddress:'0xE650ac792a9244dd9E97e37548a1123D9ba27003',
     } as Business,
     {
