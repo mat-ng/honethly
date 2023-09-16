b8c2fef1eb84949c15abd3367679ca96070f09fc
Alvin Qian <alvinhalo4@gmail.com>
2023-09-16 16:47:10 -0400

    fix header top gap

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-gradient-to-b from-zinc-200 pb-2 pt-12 backdrop-blur-2xl dark:bg-zinc-800/30 ">
+    <header className="fixed top-0 left-0 right-0 bg-gradient-to-b from-zinc-200 pb-2 pt-9 backdrop-blur-2xl dark:bg-zinc-800/30 ">
       <div className="container mx-auto px-6">
         <h1 className="font-bold text-2xl hover:text-emerald-600">honETHly
         <Button className = "float-right align-top hover:bg-emerald-600"
